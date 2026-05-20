27489014e16e44634c82f2e27900b88f13ffd114
Ankit Pandey <ankit@CELSYSBLR000333.local>
2026-05-20 11:53:57 +0530

    Password validation

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -78,21 +78,21 @@ const Signup = () => {
       .required("Mobile number is required"),
 
     password: Yup.string()
-      .required("Password is required")
-      .min(8, "Password must be at least 8 characters long")
-      .matches(
-        /[!@#$%^&*(),.?":{}|<>]/,
-        "Password must contain at least one special character",
-      )
-      .matches(
-        /^(?=.*[A-Z]).+$/,
-        "Password must contain at least one uppercase letter",
-      )
-      .matches(
-        /^(?=.*[a-z]).+$/,
-        "Password must contain at least one lowercase letter",
-      )
-      .matches(/^(?=.*[0-9]).+$/, "Password must contain at least one number"),
+      .required("Password is required"),
+      // .min(8, "Password must be at least 8 characters long")
+      // .matches(
+      //   /[!@#$%^&*(),.?":{}|<>]/,
+      //   "Password must contain at least one special character",
+      // )
+      // .matches(
+      //   /^(?=.*[A-Z]).+$/,
+      //   "Password must contain at least one uppercase letter",
+      // )
+      // .matches(
+      //   /^(?=.*[a-z]).+$/,
+      //   "Password must contain at least one lowercase letter",
+      // )
+      // .matches(/^(?=.*[0-9]).+$/, "Password must contain at least one number"),
 
     confirmPassword: Yup.string()
       .oneOf([Yup.ref("password")], "Passwords must match")
